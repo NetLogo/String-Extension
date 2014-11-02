7ce11fa3241bae16d93ec5485540f28b8699830c
arthurhjorth <arthur.hjorth@u.northwestern.edu>
2014-11-02 13:39:02 -0600

    Added to-list and from-list

---
+++ src/FromList.java
@@ -0,0 +1,34 @@
+package org.nlogo.extensions.string;
+
+import org.nlogo.api.Argument;
+import org.nlogo.api.Context;
+import org.nlogo.api.DefaultReporter;
+import org.nlogo.api.ExtensionException;
+import org.nlogo.api.LogoException;
+import org.nlogo.api.Syntax;
+import org.nlogo.api.LogoListBuilder;
+import org.nlogo.api.LogoList;
+
+public class FromList extends DefaultReporter{
+	
+	@Override
+	public Syntax getSyntax(){
+			return Syntax.reporterSyntax(
+					// we take in a string 
+					new int[] {Syntax.ListType() },
+					// and return a list
+					Syntax.StringType()); 
+	}
+
+	@Override
+	public Object report(Argument[] arg0, Context arg1)
+			throws ExtensionException, LogoException {
+		LogoList input = arg0[0].getList();
+		String theString = "";
+		for (Object o : input){
+			theString = theString + String.valueOf(o);
+		}
+		return theString;
+	}
+	
+}
