0fec755f3ab1f40fa53c3985e16bce48bcae2e0b
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2023-04-12 12:06:04 -0500

    Bugfix: Properly generate md5 strings with leading 0's

--- src/main/MessageDigest5.scala
+++ src/main/MessageDigest5.scala
@@ -1,12 +1,14 @@
 package org.nlogo.extensions.string
 
+import java.lang.Integer
+import java.security.{ MessageDigest, NoSuchAlgorithmException }
+
 import org.nlogo.api.Argument
 import org.nlogo.api.Context
 import org.nlogo.api.Reporter
 import org.nlogo.api.ExtensionException
 import org.nlogo.api.LogoException
 import org.nlogo.core.Syntax
-import java.security._
 
 class MessageDigest5 extends Reporter {
 
@@ -30,8 +32,12 @@ class MessageDigest5 extends Reporter {
       val messageDigest: Array[Byte] = algorithm.digest()
       val hexString = new StringBuffer()
       for (i <- 0 until messageDigest.length) {
-        hexString.append(
-          java.lang.Integer.toHexString(0xFF & messageDigest(i)))
+        val iVal = 0xFF & messageDigest(i)
+        if (iVal < 0x10) {
+          hexString.append(s"0${Integer.toHexString(iVal)}")
+        } else {
+          hexString.append(Integer.toHexString(iVal))
+        }
       }
       result = hexString.toString
     } catch {
